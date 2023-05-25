223d2eb7f242536149d2e3e10a2827599cf9b70e
PGD <rudeh1253@gmail.com>
2023-05-25 14:17:51 +0900

    feat: Add position parameter

--- src/components/Vertex.js
+++ src/components/Vertex.js
@@ -2,8 +2,9 @@ import './Vertex';
 
 function Vertex(props) {
     const name = props.name;
+    const position = props.position;
     return (
-        <div className="vertex">
+        <div className="vertex" style={ { top: position.y, left: position.x } }>
             <p>{ name }</p>
         </div>
     )
