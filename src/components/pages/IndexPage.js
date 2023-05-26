abcd0cd73b19662dcc4d8a147e269d93ac2d18b5
pgd <rudeh1253@gmail.com>
2023-05-26 16:26:20 +0900

    feat: Add a function - Adding a vertex

--- src/components/pages/IndexPage.js
+++ src/components/pages/IndexPage.js
@@ -1,6 +1,16 @@
+import { useState } from "react";
 import "./IndexPage.css";
+import Vertex from "../Vertex";
 
 function IndexPage() {
+    const [ vertexSet, setVertexSet ] = useState([ ]);
+    const [ inputName, setInputName ] = useState("");
+    const defaultPosition = { x: 100, y: 100 };
+    const addVertex = (name) => {
+        const newVertexSet = [ ...vertexSet ];
+        newVertexSet.push(<Vertex name={ name } position={ defaultPosition }/>);
+        setVertexSet(newVertexSet);
+    }
     return (
         <div className="container">
             <header>
@@ -8,10 +18,18 @@ function IndexPage() {
             </header>
             <section>
                 <div>
-                    <button className="btn">Add vertex</button>
+                    <input type="text" onChange={ (e) => setInputName(e.target.value) }/>
+                    <button className="btn" onClick={ (e) => {
+                        console.log("here");
+                        const name = inputName;
+                        setInputName("");
+                        addVertex(name);
+                    } }>Add vertex</button>
                 </div>
                 <div>
-                    (Graph will be presented here)
+                    {
+                        vertexSet.map((elem) => elem)
+                    }
                 </div>
             </section>
         </div>
