e518d4bc50999fdcd7c4bbaae7de6301f6cd359a
pgd <rudeh1253@gmail.com>
2023-05-26 20:12:20 +0900

    fix: Key is not a prop, so modify to the name id

--- src/components/Vertex.js
+++ src/components/Vertex.js
@@ -4,11 +4,11 @@ function Vertex(props) {
     const name = props.name;
     const position = props.position;
     const onClick = props.onClick;
-    const key = props.key;
+    const id = props.id;
     return (
         <div className="vertex" style={ { top: `${position.y}px`, left: `${position.x}px` } }
              onClick={ onClick }>
-            <p style={ { display: "none" } }>{ key }</p>
+            <p style={ { display: "none" } }>{ id }</p>
             <p>{ name }</p>
         </div>
     )
