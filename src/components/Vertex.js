01921aaf8dc40af6b70ad505130f5d9d1f1b0e87
pgd <rudeh1253@gmail.com>
2023-05-26 20:09:41 +0900

    fix: Add key to the component

--- src/components/Vertex.js
+++ src/components/Vertex.js
@@ -4,9 +4,11 @@ function Vertex(props) {
     const name = props.name;
     const position = props.position;
     const onClick = props.onClick;
+    const key = props.key;
     return (
         <div className="vertex" style={ { top: `${position.y}px`, left: `${position.x}px` } }
              onClick={ onClick }>
+            <p style={ { display: "none" } }>{ key }</p>
             <p>{ name }</p>
         </div>
     )
