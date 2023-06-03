34254b17a0ea4a5960db67b0cc7e881c6bb1cc75
PGD <rudeh1253@gmail.com>
2023-06-03 15:22:42 +0900

    docs: modify the specification of getPath function

--- src/helper/edgeHelper.js
+++ src/helper/edgeHelper.js
@@ -2,8 +2,13 @@
  * Get path from sPos to tPos,
  * following the format of "d" attribute of <path> tag.
  * The path consists of straight lines, allowing at most two
- * right angle curves
- * e.g. "M0,0 L100,0 L100,50 L200,50"
+ * right angle curves.
+ * If vertical displace is bigger than horizontal displace,
+ * the edge starts to go forward vertically, otherwise
+ * horizontally.
+ * The first curve appears at the middle spot.
+ * e.g. If sPos = (0, 0) and tPos = (200, 50)
+ * "M0,0 L100,0 L100,50 L200,50"
  * @param {*} sPos the position (x, y) of source vertex
  * @param {*} tPos the position (x, y) of target vertex
  * @returns path
