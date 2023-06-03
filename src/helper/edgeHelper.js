fbe0a8337dc7e65cd1691db9ce7e25eb1d9c09a2
PGD <rudeh1253@gmail.com>
2023-06-03 15:17:20 +0900

    delete: remove addEdge function || add: add getPath function

--- src/helper/edgeHelper.js
+++ src/helper/edgeHelper.js
@@ -1,4 +1,13 @@
-export function addEdge(from, to) {
-    console.log("from:", from);
-    console.log("to:", to);
+/**
+ * Get path from sPos to tPos,
+ * following the format of "d" attribute of <path> tag.
+ * The path consists of straight lines, allowing at most two
+ * right angle curves
+ * e.g. "M0,0 L100,0 L100,50 L200,50"
+ * @param {*} sPos the position (x, y) of source vertex
+ * @param {*} tPos the position (x, y) of target vertex
+ * @returns path
+ */
+export function getPath(sPos, tPos) {
+    return null;
 }
